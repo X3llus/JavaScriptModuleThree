70c33209973ac953824d1d117326bfe95e186692
xellus <braden.coates@hotmail.com>
2020-03-07 14:20:13 -0500

    made basic files for website

---
+++ server.js
@@ -0,0 +1,30 @@
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------//
+// Encryption library things
+const bcrypt = require('bcrypt');
+const saltRounds = 12;
+
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------//
+// Code to listen for requests using Express Framework
+const express = require('express');
+const bodyParser = require('body-parser')
+const app = express();
+
+app.use(express.static(__dirname + '/Frontend', {
+  extensions: ['html']
+}));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+app.use(bodyParser.json());
+
+app.listen(8058, () => console.log("listening on port 8058"));
+
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------//
+// MySQL connection
+const mysql = require('mysql');
+var pool = mysql.createPool({
+  connectionLimit: 100,
+  host: "localhost",
+  user: "server",
+  password: ""
+});
