241b4c82075075e3b8b04f601bdd2a89c22d40ba
xellus <braden.coates@hotmail.com>
2020-03-09 17:42:55 -0400

    started server.js

--- server.js
+++ server.js
@@ -21,10 +21,12 @@ app.listen(8058, () => console.log("listening on port 8058"));
 
 //-----------------------------------------------------------------------------------------------------------------------------------------------------------//
 // MySQL connection
+var auth = require('./auth.json');
+
 const mysql = require('mysql');
 var pool = mysql.createPool({
   connectionLimit: 100,
   host: "localhost",
-  user: "server",
-  password: ""
+  user: auth.username,
+  password: auth.password
 });
