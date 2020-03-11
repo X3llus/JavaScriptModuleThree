8d965edfc39285930163424e75d6af34a1ac6a78
xellus <braden.coates@hotmail.com>
2020-03-11 19:36:05 -0400

    renamed some stuff

--- app.js
+++ app.js
@@ -1,32 +1,34 @@
 //-----------------------------------------------------------------------------------------------------------------------------------------------------------//
-// Encryption library things
+// Security stuff
 const bcrypt = require('bcrypt');
 const saltRounds = 12;
+// const auth = require('./auth.json');
 
 //-----------------------------------------------------------------------------------------------------------------------------------------------------------//
 // Code to listen for requests using Express Framework
 const express = require('express');
+const index = require('./routes/index.js');
 const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
 const app = express();
 
-app.use(express.static(__dirname + '/Frontend', {
-  extensions: ['html']
-}));
+app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({
   extended: false
 }));
 app.use(bodyParser.json());
 
-app.listen(8058, () => console.log("listening on port 8058"));
+app.use('/', index);
+
+app.listen(8080, () => console.log("listening on port 8080"));
 
 //-----------------------------------------------------------------------------------------------------------------------------------------------------------//
 // MySQL connection
-var auth = require('./auth.json');
-
-const mysql = require('mysql');
-var pool = mysql.createPool({
-  connectionLimit: 100,
-  host: "localhost",
-  user: auth.username,
-  password: auth.password
-});
+// const mysql = require('mysql');
+// var pool = mysql.createPool({
+//   connectionLimit: 100,
+//   host: "localhost",
+//   user: auth.username,
+//   password: auth.password
+// });
