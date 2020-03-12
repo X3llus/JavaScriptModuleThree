70d09f654a3112bb1f6166312d989ad93e96bcda
xellus <braden.coates@hotmail.com>
2020-03-11 23:24:34 -0400

    made a database file for the pool

---
+++ routes/database.js
@@ -0,0 +1,9 @@
+const auth = require("../auth.json");
+const mysql = require("mysql");
+exports.pool = mysql.createPool({
+  connectionLimit: 100,
+  host: "localhost",
+  user: auth.username,
+  password: auth.password,
+  database: "jsm3"
+});
